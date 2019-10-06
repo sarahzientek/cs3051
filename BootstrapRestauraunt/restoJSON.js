8ff2749258a1159295f635151ec0f79cb3307506
sarahzientek <a99742@aup.edu>
2019-10-06 15:40:48 +0000

    updates

---
+++ BootstrapRestauraunt/restoJSON.js
@@ -0,0 +1,14 @@
+{"MyRestoPages":
+[
+    {"name":"MainPage",
+    "info":["A NavBar that allows you to access the other pages","The main title","carousel with three images"]},
+    {"name":"Menu",
+    "info":["images 8 types of water and three sides, modal that when you click on image its price comes up"]},
+    {"name":"AboutUs",
+    "info":["Information about the owner","informational video about water"]},
+    {"name":"WhereToFindUs",
+    "info":["a map of 3 locations in Paris","three addresses"]},
+    {"name":"Reservations",
+    "info":["A reservation page","first and last name blanks","drop down time chooser","drop down date chooser","done button"]}
+    ]
+}
