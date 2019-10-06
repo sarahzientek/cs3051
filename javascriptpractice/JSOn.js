77ae2fa0e0a6047de31117e215ece47504544c13
sarahzientek <a99742@aup.edu>
2019-10-06 12:03:37 +0000

    updates

---
+++ javascriptpractice/JSOn.js
@@ -0,0 +1,10 @@
+let myObj = {
+    name: "sarah",
+    age: 15
+    print: function() {
+
+    }
+}
+
+let json = //some JSON
+let jsObj = JSON.parson(json)
---
+++ vuemenu/src/components/header.vue
@@ -0,0 +1,3 @@
+<template>
+<h1> OUR MENU </h1>
+</template>
